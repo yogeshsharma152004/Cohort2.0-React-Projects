f12cf000764044f8161982327a00fdd596537486
sharmayogesh152004@gmail.com <sharmayogesh152004@gmail.com>
2026-03-05 19:00:19 +0530

    project-7

---
+++ Project-7/src/App.jsx
@@ -0,0 +1,60 @@
+
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./components/Card";
+
+
+
+const App = () => {
+
+  const [allData, setAllData] = useState([])
+
+  const getData = async () => {
+
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=12')
+
+
+
+    const list = response.data.results
+
+    // console.log(list);
+    
+
+      const details = await  Promise.all(
+        list.map(async (elem) => {
+           const results = await axios.get(elem.url)
+           
+           
+           
+           return results.data
+           
+           
+           
+        })
+      )
+
+
+      setAllData(details)   
+
+      console.log(details);
+      
+  }
+
+  useEffect( () => {
+    getData()
+  },[])
+
+  return (
+    <div className="bg-black h-auto p-20 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-6   md:gap-14 items-center justify-center gap-6  text-white ">
+        
+
+        {allData.map((elem,index) => {
+         return <Card key={index} elem={elem} />
+          
+        })}
+    </div>
+  )
+}
+
+export default App
---
+++ Project-7/src/components/Card.jsx
@@ -0,0 +1,94 @@
+
+
+
+// import React from "react";
+
+// const Card = ({ pokemon }) => {
+//   return (
+//     <div className="card">
+
+//       <img
+//         src={pokemon.sprites.front_default}
+//         alt={pokemon.name}
+//       />
+
+//       <h2>{pokemon.name}</h2>
+
+
+
+//       <p>
+//         <b>Type:</b> {pokemon.types[0].type.name}
+//       </p>
+
+//       <p>
+//         <b>Height:</b> {pokemon.height}
+//       </p>
+
+//       <p>
+//         <b>Weight:</b> {pokemon.weight}
+//       </p>
+
+//     </div>
+//   );
+// };
+
+// export default Card;
+
+import React from 'react'
+import { CiDumbbell } from "react-icons/ci";
+import { FaFire } from "react-icons/fa";
+import { MdWaterDrop } from "react-icons/md";
+import { GiLindenLeaf } from "react-icons/gi";
+import { IoBug } from "react-icons/io5";
+
+
+const Card = (promise) => {
+
+  const typeColors = {
+  fire: "bg-red-500",
+  water: "bg-blue-500",
+  grass: "bg-green-500",
+  bug: "bg-lime-500",
+
+};
+
+const typeIcons = {
+  fire: <FaFire size={20}/>,
+  water: <MdWaterDrop size={20} />,
+  grass: <GiLindenLeaf size={20} />,
+  bug:  < IoBug size={20} />
+};
+
+const typeGradient = {
+    grass: "from-green-500/70 to-green-800",
+  fire: "from-orange-500/70 to-red-800",
+  water: "from-blue-500/70 to-blue-800",
+  bug: "from-lime-400/70 to-lime-700"
+  
+};
+
+const type = promise.elem.types[0].type.name;
+
+  return (
+    <div className={`card h-[350px] w-[300px] capitalize bg-amber-300 rounded-lg font-mono  flex flex-col items-center bg-gradient-to-b ${typeGradient[type]}`}> 
+    <img className='h-40 w-40  ' src={promise.elem.sprites.front_default} alt="" />
+   
+         <h1 className='font-semibold text-3xl text-black '>  {promise.elem.name} </h1> 
+
+         <p className={`flex items-center gap-2 capitalize mt-2 font-medium text-2xl  font-semibold  px-5 py-1 rounded-lg ${typeColors[promise.elem.types[0].type.name]}`}> {typeIcons[type]}
+  {type}</p>
+
+         
+
+         <div className='flex gap-5 items-center mt-6'>
+            
+            <p className='bg-[#00000069] px-5 py-1 rounded-lg text-xl'> <i className="ri-ruler-line"></i>  {promise.elem.height /10}M    </p>
+
+         <p className='flex  items-center gap-2 bg-[#00000069] px-5 py-1 rounded-lg text-xl '> <CiDumbbell />{promise.elem.weight / 10}Kg</p>
+          
+         </div>
+    </div>
+  )
+}
+
+export default Card
