0ab9514305c19c27da504fdf6677f96da2ca9e6d
sharmayogesh152004@gmail.com <sharmayogesh152004@gmail.com>
2026-03-06 11:54:16 +0530

    update

--- Project-7/src/App.jsx
+++ Project-7/src/App.jsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./components/Card";
+import Poke from "./assets/poke.png"
 
 
 
@@ -47,7 +48,7 @@ const App = () => {
 
   return (
     <div className="bg-black h-auto flex flex-col items-center">
-      <img className="w-40 " src="./src/assets/poke.png" alt="" />
+      <img className="w-40 " src={Poke} alt="" />
       <div className=" p-20 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-6   md:gap-14 items-center justify-center gap-10  text-white ">
           
           
